61120cccbe4facf1b8977516b80f29b34600808f
wouter-codes <wouter.klinkenberg@outlook.com>
2026-03-15 18:06:00 +0000

    Add Procfile, install gunicorn

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '.127.0.0.1',]
 
 CSRF_TRUSTED_ORIGINS = ['https://*.herokuapp.com']
 
